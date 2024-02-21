727e31c615119d5e1e0b588b52e6b84a06f23c1f
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-21 18:41:31 +0100

    Add project files.

---
+++ inlämning6.12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        Triangel(x);
+    }
+    static void Triangel(int x)
+    {
+        for (int i = x; i >0; i--)
+        {
+            for (int j = i-1; j >0; j--)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("*");
+            int k = x - i;
+            for (int p = 0; p <=2*k-1; p++)
+            {
+                Console.Write("*");
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
